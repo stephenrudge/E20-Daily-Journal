47529b05e7f13cf7b1943e82b52a96335f252b05
stephenrudge <sprock.ryder@gmail.com>
2022-09-09 21:42:31 -0500

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import {getEntries} from './entries.js'
+import {addNewEntry, getEntries} from './entries.js'
 
 const displayJournalEntries = () =>{
     const journalEntries = getEntries()
@@ -9,7 +9,7 @@ const displayJournalEntries = () =>{
     html+= `
     <div class= "journalEntry">
     <p>Date: ${journalEntries[i].dateOfEntry}</p>
-    <p>Type of travel: ${journalEntries[i].conceptsCovered}</p>
+    <p>Type of travel: ${journalEntries[i].typesOfTravel}</p>
     <p>Journal Entry: ${journalEntries[i].journalEntry}</p>
     <p>Mood of the Day: ${journalEntries[i].moodForTheDay}</p>
     </div>`
@@ -17,3 +17,33 @@ const displayJournalEntries = () =>{
     document.getElementById("entries").innerHTML= html 
 }
 displayJournalEntries()
+
+
+document.addEventListener("click", (event) =>{
+    if(event.target.id == "submitNewQuote"){
+        // e.preventDefault()
+        const dateOfEntry = document.querySelector("input[name=entryDate]")?.value;
+        const typesOfTravel = document.getElementById('travelType')?.value;
+        console.log(typesOfTravel)
+        const journalEntry = document.getElementById('journalEntry')?.value;
+        const moodForTheDay  = document.querySelector("select[name=cars]")?.value;
+
+        
+ let dateEntered = new Date(dateOfEntry)
+dateEntered = dateEntered.toLocaleDateString()
+ 
+const newQuote = {
+            id: 1,
+            dateOfEntry: dateEntered,
+            typesOfTravel: typesOfTravel,
+            journalEntry: journalEntry,
+            moodForTheDay: moodForTheDay,   
+        }
+              addNewEntry(newQuote)
+    }
+});
+
+document.addEventListener("stateChanged", (event) => {
+    displayJournalEntries();
+  });
+  
